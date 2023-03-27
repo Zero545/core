a709f53ec03645c27fddb1dc0aea78bda2b12cb1
Eduard Weis <eduard-weis@web.de>
2023-03-27 20:08:25 +0000

    Created gateway wrapper class

--- homeassistant/components/haus_bus/__init__.py
+++ homeassistant/components/haus_bus/__init__.py
@@ -6,6 +6,7 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
+from .gateway import HausbusGateway
 
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
@@ -20,7 +21,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # TODO 1. Create API instance
     # TODO 2. Validate the API connection (and authentication)
     # TODO 3. Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
+
+    hass.data[DOMAIN][entry.entry_id] = HausbusGateway(hass, entry)
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
---
+++ homeassistant/components/haus_bus/gateway.py
@@ -0,0 +1,12 @@
+"""Representation of a Haus-Bus gateway."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+
+class HausbusGateway:
+    """Manages a single Haus-Bus gateway."""
+
+    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+        """Initialize the system."""
+        self.hass = hass
+        self.config_entry = config_entry
